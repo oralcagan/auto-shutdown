8b3c02ae8e9b1e46f7cb1023b5af5d278f391682
oralcagan <oralcagan@gmail.com>
2021-07-28 21:20:33 +0300

    Small optimizations

--- src/main.rs
+++ src/main.rs
@@ -27,14 +27,14 @@ To get the name of the game, the program requests the html page https://store.st
 The name of the game is in this html tag: <div id="appHubAppName" class="apphub_AppName">{GAME_NAME}</div>,
 So, the program reads from the http response stream until it matches 'class="apphub_AppName">', then it reads the stream and stores the data until it matches '</div>'.
 
-To check if the game -or an update- is downloaded or not, this looks for the folders inside DOWNLOAD_FOLDER. 
-If the folder whose name is the id of the game isn't there, it concludes that the game has been downloaded. 
+To check if the game -or an update- is downloaded or not, this looks for the folders inside DOWNLOAD_FOLDER.
+If the folder whose name is the id of the game isn't there, it concludes that the game has been downloaded.
 
 You might have to change these constants for your own computer.
 */
 
 ///Explained above
-const APP_NAME_PREFIX: &[u8] = b"apphub_AppName\">";
+const APP_NAME_PREFIX: &[u8] = b"<div id=\"appHubAppName\" class=\"apphub_AppName\">";
 const APP_NAME_SUFFIX: &[u8] = b"</div>";
 /// App page url
 const APP_URL: &str = "https://store.steampowered.com/app/";
@@ -45,7 +45,7 @@ const DOWNLOAD_FOLDER: &str = "C:\\Program Files (x86)\\Steam\\steamapps\\downlo
 /// Determines how frequently the program should check the "downloading" folder. The interval time is in seconds.
 const CHECK_INTERVAL_SEC: u64 = 30;
 /// Shutdown command name
-const SHUTDOWN_CMD : &str = "shutdown";
+const SHUTDOWN_CMD: &str = "shutdown";
 /// Shutdown command args
 const SHUTDOWN_ARGS: &[&str] = &["/s"];
 
@@ -98,7 +98,10 @@ fn get_game_name(id: &str) -> String {
 fn req_game_page(id: &str) -> Result<impl io::Read + Send, ureq::Error> {
     let mut url = APP_URL.to_owned();
     url.push_str(id);
-    let res = ureq::get(&url).set("Cookie", COOKIE_VAL);
+    let res = ureq::get(&url)
+        .set("Cookie", COOKIE_VAL)
+        .set("Connection", "Keep-Alive")
+        .set("Keep-Alive", "timeout=30");
     match res.call() {
         Ok(r) => return Ok(r.into_reader()),
         Err(e) => return Err(e),
@@ -108,17 +111,18 @@ fn req_game_page(id: &str) -> Result<impl io::Read + Send, ureq::Error> {
 fn reader_match_exp(exp: &[u8], reader: &mut impl io::Read) -> bool {
     let l = exp.len();
     let mut buf = vecdeque_with_size(l);
-    res_match!(reader.read_exact(buf.make_contiguous()), return false);
-    if buf == exp {
+    let bufref = buf.make_contiguous();
+    res_match!(reader.read_exact(bufref), return false);
+    if bufref == exp {
         return true;
     }
     loop {
-        buf.rotate_left(1);
+        bufref.rotate_left(1);
         res_match!(
-            reader.read_exact(&mut buf.make_contiguous()[l - 1..l]),
+            reader.read_exact(&mut bufref[l - 1..l]),
             return false
         );
-        if buf == exp {
+        if bufref == exp {
             return true;
         }
     }
@@ -128,22 +132,23 @@ fn reader_read_and_store_until(exp: &[u8], reader: &mut impl io::Read) -> Option
     let mut v = Vec::new();
     let l = exp.len();
     let mut buf = vecdeque_with_size(l);
-    res_match!(reader.read_exact(buf.make_contiguous()), return None);
-    if buf == exp {
+    let bufref = buf.make_contiguous();
+    res_match!(reader.read_exact(bufref), return None);
+    if bufref == exp {
         return None;
     }
-    v.extend_from_slice(buf.make_contiguous());
+    v.extend_from_slice(bufref);
     loop {
-        buf.rotate_left(1);
+        bufref.rotate_left(1);
         res_match!(
-            reader.read_exact(&mut buf.make_contiguous()[l - 1..l]),
+            reader.read_exact(&mut bufref[l - 1..l]),
             return None
         );
-        if buf == exp {
+        if bufref == exp {
             rem_n_from_end(&mut v, l - 1);
             return Some(v);
         }
-        v.extend_from_slice(&mut buf.make_contiguous()[l - 1..l]);
+        v.extend_from_slice(&mut bufref[l - 1..l]);
     }
 }
 
@@ -170,12 +175,9 @@ fn look_for_folder(folder: &str) -> bool {
     for f in folder_iter {
         let entry = f.expect("Couldn't read the folder");
         let osstr = entry.file_name();
-        let is_dir = entry.metadata().expect("Couldn't read the folder").is_dir();
-        if is_dir {
-            let a_folder = osstr.to_str().expect("Couldn't read the folder");
-            if a_folder == folder {
-                return true;
-            }
+        let a_folder = osstr.to_str().expect("Couldn't read the folder");
+        if a_folder == folder {
+            return true;
         }
     }
     return false;
